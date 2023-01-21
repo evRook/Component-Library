32166e2a23339ee0cfb26cf494ff78431b9148ce
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-21 12:31:02 -0700

    File Setup and test

---
+++ lib/src/stories/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Button(props) {
+  return ( 
+    <button className="primary">Button</button>
+   );
+}
+
+export default Button;
---
+++ lib/src/stories/Button.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from './Button'
+import './button.css'
+
+export default {
+    title: 'Button',
+    component: Button,
+}
+
+export const Primary = () => <Button />;
+export const Danger = () => <Button />;
+export const Success = () => <Button />
+export const Warning = () => <Button />
+export const Default = () => <Button />
+export const AddToCart = () => <Button />
+export const CartIcon = () => <Button />
+export const AddToFavorites = () => <Button />
+export const FavoritesIcon = () => <Button />
