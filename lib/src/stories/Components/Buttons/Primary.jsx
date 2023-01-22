cf5bc7f592f248452354c0e6d954960dc53cc451
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-22 12:07:06 -0700

    working on button toggle

--- lib/src/stories/Components/Buttons/Primary.jsx
+++ lib/src/stories/Components/Buttons/Primary.jsx
@@ -14,24 +14,24 @@ export default function Button({Type, Text, Size = "Medium"}) {
     transitionDuration: "0.1s",
   }
 
-  // let visited = false
+  let visited = false
 
-  // function Test(){
-  //   if(visited === true){
-  //     console.log("true")
-  //     visited = false
-  //   }else if(visited === false){
-  //     console.log("false")
-  //     visited = true
-  //   }
-  // }
+  function Test(){
+    if(visited === true){
+      console.log("true")
+      visited = false
+    }else if(visited === false){
+      console.log("false")
+      visited = true
+    }
+  }
 
   return ( 
     <button 
       className={"global ".concat(Type)} 
       Size={Size} 
       style={style} 
-      // onClick={Test}
+      onClick={Test}
     >
       {Text}
     </button>
