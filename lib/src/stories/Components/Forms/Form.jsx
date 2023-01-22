ec805836d05f8ece715093b4ca4f36921f880c03
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-22 14:08:13 -0700

    form file set up

---
+++ lib/src/stories/Components/Forms/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Form(props) {
+    return ( 
+        <p className={props.className}>Hello World</p>
+     );
+}
+
+export default Form;
--- lib/src/stories/Form.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Form() {
-    return ( 
-        <p>Hello World</p>
-     );
-}
-
-export default Form;
