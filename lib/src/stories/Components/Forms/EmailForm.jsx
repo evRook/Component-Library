3866bd86607db35e7ce7871df38a2e21ae6322df
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-23 14:41:19 -0700

    email form size change functionality

--- lib/src/stories/Components/Forms/EmailForm.jsx
+++ lib/src/stories/Components/Forms/EmailForm.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-export default function EmailForm(props) {
+export default function EmailForm({Size = "Small"}) {
+
+    let x = 1
+    if(Size === "Small"){
+        x = 0.5
+    }else if(Size === "Large"){
+        x = 1.5
+    } 
+
+    let style = {
+        padding: `${x * 12}px ${x * 25}px ${x * 12}px 15px`,
+    }
+
     return ( 
         <div className="email--container">
             <label for="emailInput">Email</label>
-            <input type="text" placeholder="Email" id="emailInput"/>
+            <input type="text" placeholder="Email" id="emailInput" Size={Size} style={style}/>
         </div>
-     );
+    );
 }
--- lib/src/stories/EmailForm.stories.jsx
+++ lib/src/stories/EmailForm.stories.jsx
@@ -5,6 +5,20 @@ import './Components/Forms/EmailForm.css'
 export default {
     title: 'Forms',
     component: EmailForm,
+    argTypes: {
+        Size: {
+            options: ['Small', 'Medium', 'Large'],
+            control: { type: 'inline-radio'},
+        },
+    }
 }
 
-export const Email = () => <EmailForm className="test"/>
+// export const Email = () => <EmailForm className="test"/>
+
+const Template = (args) => <EmailForm {...args} />
+
+export const Email = Template.bind({})
+
+Email.args = {
+    Size: "Small",
+}
