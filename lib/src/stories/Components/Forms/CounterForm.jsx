58d871a1ce82c82b5b1f4b47215ae6f28c88139f
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-23 11:39:11 -0700

    counterform styling and frame

--- lib/src/stories/Components/Forms/CounterForm.jsx
+++ lib/src/stories/Components/Forms/CounterForm.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 export default function CounterBtn(props) {
+
+    let x = 1
+    const style = {
+        padding: `${x * 12}px ${x * 25}px`,
+    }
+
+    let count = 0
+
     return ( 
-        <p className={props.className}>Hello World</p>
+        <div className="counter--container">
+            <div>
+                <button onClick={() => {console.log("test-")}}>-</button>
+            </div>
+            <div>
+                <p>{count}</p>
+            </div>
+            <div>
+                <button onClick={() => {console.log("test+")}}>+</button>
+            </div>
+        </div>
      );
 }
