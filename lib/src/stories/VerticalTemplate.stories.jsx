427534cbeb6280aa06497d957258e55868d5ca67
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-23 20:05:10 -0700

    vertical template file set up

---
+++ lib/src/stories/VerticalTemplate.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import VerticalTemplate from './Components/Templates/VerticalTemplate'
+import './Components/Templates/VerticalTemplate.css'
+
+export default {
+    title: 'Templates',
+    component: VerticalTemplate,
+}
+
+export const Vertical = () => <VerticalTemplate className="testVertical" />
