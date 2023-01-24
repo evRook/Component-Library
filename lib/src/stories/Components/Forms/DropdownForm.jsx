d3e89d2fa7cca2f84df98353f7f55343006aa530
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-23 17:48:15 -0700

    select dropdown wire frame

--- lib/src/stories/Components/Forms/DropdownForm.jsx
+++ lib/src/stories/Components/Forms/DropdownForm.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-export default function DropdownForm(props) {
+export default function DropdownForm({Text}) {
     return ( 
-        <p className={props.className}>Hello World</p>
+        <div className="dropdown--container">
+            <select id="dropdown">
+                <option value="test" disable selected hidden>Test</option>
+                <option value="test">Default</option>
+                <option value="test">Primary</option>
+                <option value="test">Danger</option>
+                <option value="test">Success</option>
+                <option value="test">Warning</option>
+            </select>
+        </div>
      );
 }
