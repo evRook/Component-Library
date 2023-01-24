bca28fc97a1302ae661ee2d790ee4fc2fea8fdbc
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-23 19:22:32 -0700

    voucher wire and styling start

--- lib/src/stories/Components/Forms/VoucherForm.jsx
+++ lib/src/stories/Components/Forms/VoucherForm.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function VoucherForm(props) {
     return ( 
-        <p className={props.className}>Hello World</p>
+        <div className="voucher--container">
+            <input type="text" id="voucher" placeholder="Voucher Code"/>
+            <label for="voucher">Redeem</label>
+        </div>
      );
 }
