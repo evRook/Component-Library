97610ec4b1a5f702947d1d3251849f67c0840353
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-23 20:32:30 -0700

    vretical template wire and css "wire"

--- lib/src/stories/Components/Templates/VerticalTemplate.jsx
+++ lib/src/stories/Components/Templates/VerticalTemplate.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import macBook from './img/50061505_575246.png'
 
-export default function VerticalTemplate({className}) {
+export default function VerticalTemplate({props}) {
     return (
-      <p className={className}>TESTTESTEST</p>  
+      <div className="vertical--container">
+        <div className="vertical--img">
+            <img src={macBook} alt="" />
+        </div>
+        <div className="vertical--lineBreak"></div>
+        <p className="vertical--title">Apple MacBook Pro</p>
+        <div className="vertical--rating__container">
+            
+        </div>
+        <div className="vertical--price__container">
+            <p className="vertical--sale">$499</p>
+            <p className="vertical--msrp">$599</p>
+        </div>
+      </div> 
     );
 }   
